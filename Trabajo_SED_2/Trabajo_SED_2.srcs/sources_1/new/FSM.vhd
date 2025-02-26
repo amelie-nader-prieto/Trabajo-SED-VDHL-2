2d775fb2ff7e52e40439f065f5adcd0d7cbacbea
Amélie <amelie.nader.prieto@alumnos.upm.es>
2025-02-26 12:06:38 +0100

    Crear FSM.vhd

---
+++ Trabajo_SED_2/Trabajo_SED_2.srcs/sources_1/new/FSM.vhd
@@ -0,0 +1,49 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity FSM is
+    port(
+        monedas : in std_logic_vector(3 downto 0);
+        comprar_producto : in std_logic;
+        reset : in std_logic;
+        clk : in std_logic;
+        importe_total : out integer;
+        entregar_producto : out std_logic;
+        error : out std_logic
+        
+    );
+end entity;
+
+architecture Behavioral of FSM is
+
+    type states is (
+        reposo,
+        recibiendo_monedas,
+        entregando_producto,
+        devolver_dinero
+    );
+    
+    signal current_state : states := reposo;
+    signal next_state : states := current_state;
+
+begin
+    -- Actualizar el estado
+    state_reg : process(clk)
+    begin
+    end process;
+    
+    -- Decodificar el estado siguiente
+    -- (depende del estado actual y de las entradas)
+    nextstate_decod : process(current_state, monedas, comprar_producto, reset)
+    begin
+    end process;
+    
+    -- Decodificar las salidas
+    -- (dependen sólo del estado actual)
+    output_decod : process(current_state)
+    begin
+    end process;
+
+end architecture;
+
