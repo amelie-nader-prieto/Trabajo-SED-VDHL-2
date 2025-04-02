391592ca64991c8e05458b9b56d08309a44410e3
SergioCantalejoVillasante <80417134+SergioCantalejoVillasante@users.noreply.github.com>
2025-04-02 13:37:38 +0200

    Update FSM_tb.vhd

--- Trabajo_SED_2/Trabajo_SED_2.srcs/sim_1/new/FSM_tb.vhd
+++ Trabajo_SED_2/Trabajo_SED_2.srcs/sim_1/new/FSM_tb.vhd
@@ -1,43 +1,96 @@
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use ieee.numeric_std.all;
+use IEEE.Std_logic_1164.all;
+use IEEE.Numeric_Std.all;
 
 entity FSM_tb is
---  Port ( );
-end FSM_tb;
+end;
 
-architecture Behavioral of FSM_tb is
+architecture bench of FSM_tb is
 
-    component FSM is
-        port(
-            monedas : in std_logic_vector(3 downto 0);
-            comprar_producto : in std_logic;
-            reset : in std_logic;
-            clk : in std_logic;
-            importe_total : out integer;
-            entregar_producto : out std_logic;
-            led_error : out std_logic
-        );
-    end component;
-    
-    signal monedas : std_logic_vector := "0000";
-    signal comprar_producto : std_logic := '0';
-    signal reset : std_logic := '0';
-    signal clk : std_logic := '0';
-    signal importe_total : integer := 0;
-    signal entregar_producto : std_logic := '0';
-    signal led_error : std_logic := '0';
+  component FSM
+      port(
+          monedas : in std_logic_vector(3 downto 0);
+          comprar_producto : in std_logic;
+          reset : in std_logic;
+          clk : in std_logic;
+          importe_total : out integer;
+          entregar_producto : out std_logic;
+          led_error : out std_logic
+      );
+  end component;
+
+  signal monedas: std_logic_vector(3 downto 0);
+  signal comprar_producto: std_logic;
+  signal reset: std_logic;
+  signal clk: std_logic;
+  signal importe_total: integer;
+  signal entregar_producto: std_logic;
+  signal led_error: std_logic ;
+
+  constant clock_period: time := 10 ns;
+  signal stop_the_clock: boolean;
 
 begin
 
-    uut : FSM port map(
-        monedas => monedas,
-        comprar_producto => comprar_producto,
-        reset => reset,
-        clk => clk,
-        importe_total => importe_total,
-        entregar_producto => entregar_producto,
-        led_error => led_error
-    );
+  uut: FSM port map ( monedas           => monedas,
+                      comprar_producto  => comprar_producto,
+                      reset             => reset,
+                      clk               => clk,
+                      importe_total     => importe_total,
+                      entregar_producto => entregar_producto,
+                      led_error         => led_error );
 
-end Behavioral;
+  stimulus: process
+  begin
+
+    -- Put initialisation code here
+    reset <= '0';
+    comprar_producto <='0';
+    monedas<="0000";
+    wait for 20 ns;
+
+    -- Put test bench stimulus code here
+
+    --reposo => moneda_introducida => recibiendo_moneda
+    -- => comprar_producto =1 importe<100 =>recibiendo monedas => importe=100
+    -- =>entregando producto => reposo
+    monedas<="0001"; --10 cents
+    wait for 20 ns;
+    monedas<="0010"; --20 cents + 10 cents = 30 cents
+    wait for 20 ns;
+    monedas<="0010"; --20 cents + 30 cents = 50 cents
+    wait for 20 ns;
+    monedas<="0000";
+    wait for 20 ns;
+    
+    comprar_producto<='1'; --importe <100
+    wait for 20 ns;
+    comprar_producto<='0';
+    wait for 20 ns;
+    
+    monedas<="0100"; --50 cents + 50 cents = 100 cents
+    wait for 20 ns;
+    monedas<="0000";
+    wait for 20 ns;
+    
+    comprar_producto<='1'; --importe =100 entregando producto
+    wait for 20 ns;
+    comprar_producto<='0';
+    wait for 20 ns;
+    
+    --wait for 2.5*100_000_000 ns; --debería volver al reposo
+    
+    stop_the_clock <= true;
+    wait;
+  end process;
+
+  clocking: process
+  begin
+    while not stop_the_clock loop
+      clk <= '0', '1' after clock_period / 2;
+      wait for clock_period;
+    end loop;
+    wait;
+  end process;
+
+end;
